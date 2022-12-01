2f752bca8c878851f8759d86323454e370eda1c6
maulanadiki <maulanadiki23@gmail.com>
2022-12-01 15:57:30 +0700

    membuat form login

---
+++ resources/views/layout/login.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
+
+</head>
+
+<body style="background-color:#FEFAFB;">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center mt-5">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div align="center" class="mt-5">
+                                <img src="asset/logo/logo.png" alt="La lumiere" width="300px" height="300px">
+                                </div>
+                            </div>
+
+                            <div class="col-md-5 alasnya">
+                                <div class="kotak">
+                                    <p class="text-center" style="padding-top:30px; font-size:30px;">Login
+
+                                    <hr width="80%"></p>
+
+                                    <div class="box-login">
+                                        <i class="bi bi-person-fill"></i>
+                                        <input type="text" name="email" email  placeholder="email">
+                                    </div>
+
+                                    <div class="box-login">
+                                        <i class="bi bi-lock-fill"></i>
+                                        <input type="password" name="password" required  placeholder="Password">
+                                    </div>
+
+                                    <div class="d-flex justify-content-end mr-4"><a href="" class="text-decoration-none">Forgot Password ?</a></div>
+                                    <div class="text-center mt-3"><button class="btn btn-pink" style="">Login</button></div>
+                            </div>
+                            </div>
+                            {{-- sampe sini --}}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
